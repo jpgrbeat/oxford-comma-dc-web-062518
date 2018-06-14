c040cde3e5fb3069b20b0c2f80e72ad54142ccd9
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 14:55:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  count = 0
+  str = ""
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    while count < array.size
+      if count == (array.size - 1)
+        str += "and #{array[count]}"
+      else
+        str += "#{array[count]}, "
+      end
+      count += 1
+    end
+    str
+  end
 end
